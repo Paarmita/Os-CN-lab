938f16ec20923559721b2d1a7b37afc117712330
Paarmita <paarmita1998@gmail.com>
2019-04-21 10:47:15 +0530

    cn lab 9-10

---
+++ CN lab/lab 9/client2.c
@@ -0,0 +1,110 @@
+#include <arpa/inet.h> 
+#include <netinet/in.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <sys/socket.h> 
+#include <sys/types.h> 
+#include <unistd.h> 
+#include <dirent.h> 
+
+#define IP_PROTOCOL 0 
+#define IP_ADDRESS "127.0.0.1"		// localhost 
+#define PORT_NO 15050 
+#define NET_BUF_SIZE 32 
+#define cipherKey 'S' 
+#define sendrecvflag 0 
+
+// funtion to clear buffer 
+void clearBuf(char* b) 
+{ 
+	int i; 
+	for (i = 0; i < NET_BUF_SIZE; i++) 
+		b[i] = '\0'; 
+} 
+
+// function for decryption 
+char Cipher(char ch) 
+{ 
+	return ch ^ cipherKey; 
+} 
+
+// function to receive file 
+int recvFile(char* buf, int s) 
+{ 
+	int i; 
+	char ch; 
+	for (i = 0; i < s; i++) { 
+		ch = buf[i]; 
+		ch = Cipher(ch); 
+		if (ch == EOF) 
+			return 1; 
+		else
+			printf("%c", ch); 
+	} 
+	return 0; 
+} 
+
+int main() 
+{ 
+	int sockfd, nBytes; 
+	struct sockaddr_in addr_con; 
+	int addrlen = sizeof(addr_con); 
+	addr_con.sin_family = AF_INET; 
+	addr_con.sin_port = htons(PORT_NO); 
+	addr_con.sin_addr.s_addr = inet_addr(IP_ADDRESS); 
+	char net_buf[NET_BUF_SIZE]; 
+	FILE* fp; 
+	
+	//new addition
+	struct dirent *de;		// Pointer for directory entry 
+
+	// opendir() returns a pointer of DIR type. 
+	DIR *dr = opendir("."); 
+
+	if (dr == NULL)			// opendir returns NULL if couldn't open directory 
+	{ 
+		printf("Could not open current directory" ); 
+		return 0; 
+	} 
+
+	// for readdir() 
+	while ((de = readdir(dr)) != NULL) 
+			printf("%s\n", de->d_name); 
+
+	closedir(dr);
+	// socket() 	
+	sockfd = socket(AF_INET, SOCK_DGRAM, 
+					IP_PROTOCOL); 
+
+	if (sockfd < 0) 
+		printf("\nfile descriptor not received!!\n"); 
+	else
+		printf("\nfile descriptor %d received\n", sockfd); 
+
+	while (1) { 
+		printf("\nPlease enter file name to receive:\n"); 
+		scanf("%s", net_buf); 
+		sendto(sockfd, net_buf, NET_BUF_SIZE, 
+			sendrecvflag, (struct sockaddr*)&addr_con, 
+			addrlen); 
+
+		printf("\n---------Data Received---------\n"); 
+
+		while (1) { 
+			// receive 
+			clearBuf(net_buf); 
+			nBytes = recvfrom(sockfd, net_buf, NET_BUF_SIZE, 
+							sendrecvflag, (struct sockaddr*)&addr_con, 
+							&addrlen); 
+
+			// process 
+			if (recvFile(net_buf, NET_BUF_SIZE)) { 
+				break; 
+			} 
+		} 
+		printf("\n-------------------------------\n"); 
+	} 
+	return 0; 
+} 
+
